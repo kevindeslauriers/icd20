be597cef76441b31aa25644b2db75e9aeee30d86
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-10-16 15:11:42 -0400

    Completed Day 1 Code

---
+++ Unit Two- Writing Python Functions and Variable Scope/Day 1 - Introduction to Reusable Code with Python Functions/using-functions-examples1.py
@@ -0,0 +1,23 @@
+length = len("Happy Cat")   # len function that takes a string and returns the number of characters
+# "Happy Cat" is the argument that len function took, it returned the value 9
+
+print(length)               # print function that takes a string or any other type and prints it to the screen
+
+
+mystery = print("Happy Cat") 
+
+
+print(mystery)      # print returns nuthing (None)
+
+
+width = int(input("Please enter a width: "))  
+# "Please enter a width: " is the argument for the input function
+# it returns  what the user entered
+# that will be the argument for the int function
+# the int function will convert that into an int and return it which gets assigned to width
+
+print(round(34.6766876, 2))
+print(round(34.6766876)) # round(34.6766876) same as rounding to an int
+
+
+
--- Unit Two- Writing Python Functions and Variable Scope/Day 1 - Introduction to Reusable Code with Python Functions/using-functions-examples2.py
+++ Unit Two- Writing Python Functions and Variable Scope/Day 1 - Introduction to Reusable Code with Python Functions/using-functions-examples2.py
@@ -87,7 +87,69 @@ def length(input_data):
     if isinstance(input_data, str):
         return len(input_data)
     
+##########################################################################
 
 
+# Examples of calling the functions:
+
+# Calculate the area of a rectangle
+rectangle_area = area_of_rectangle(5.0, 3.0)
+print("Area of the rectangle: ", rectangle_area)
+
+# Check if a string contains a specific substring
+contains = contains_substring("Hello, World!", "World")
+print("String contains 'World': ", contains)
+
+# Calculate the average of three floats
+avg_three_floats = average_of_three_floats(4.5, 2.7, 8.3)
+print("Average of three floats: ", avg_three_floats)
+
+# Concatenate two strings
+concatenated_string = concatenate_strings("Hello, ", "World!")
+print("Concatenated string: ", concatenated_string)
+
+# Calculate the volume of a cube
+cube_volume = volume_of_cube(3.0)
+print("Volume of the cube: ", cube_volume)
+
+# Check if a number is positive, negative, or zero
+number_status = check_number_status(-5)
+print("Number status: ", number_status)
+
+# Calculate the circumference of a circle
+circle_circumference = circumference_of_circle(4.0)
+print("Circumference of the circle: ", circle_circumference)
+
+# Count the number of occurrences of a character in a string
+char_count = count_char_occurrences("programming", "r")
+print("Number of 'r' in 'programming': ", char_count)
+
+# Calculate the percentage of a number
+percentage_result = calculate_percentage(80, 25)
+print("25% of 80: ", percentage_result)
+
+# Find the absolute difference between two numbers
+abs_difference = absolute_difference(7.5, 3.2)
+print("Absolute difference between 7.5 and 3.2: ", abs_difference)
+
+# Capitalize the first letter of a string
+capitalized_string = capitalize_first_letter("chatgpt")
+print("Capitalized string: ", capitalized_string)
+
+# Calculate the square of a number
+number_squared = square(4)
+print("Square of 4: ", number_squared)
+
+# Find the maximum of two numbers
+maximum_number = max_of_two(8.7, 5.2)
+print("Maximum of 8.7 and 5.2: ", maximum_number)
+
+# Calculate the square root of a number
+sqrt_result = square_root(16.0)
+print("Square root of 16: ", sqrt_result)
+
+# Find the length of a string
+string_length = length("Python is fun!")
+print("Length of the string: ", string_length)
 
 

85278ac8d2bc6c77c83ab8a2d6865556f2ee1209
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-11-03 13:36:46 -0400

    mid class

--- Unit Three - Conditional Statements in Python/Day 1 - Introduction to Conditional Statements/lesson.py
+++ Unit Three - Conditional Statements in Python/Day 1 - Introduction to Conditional Statements/lesson.py
@@ -1,7 +1,23 @@
 number_of_students = 16
 average = 83
 answer = "no"
-isRaining = True
-hasRedShoes = False
+is_raining = True
+has_red_shoes = False
 sad = False
 
+# one = is to assign a value to a variable
+
+print(number_of_students > 20)      # False
+print(average > 80)                 # True
+print(answer == "yes")              # == check for equality 
+
+print(is_raining == True)            # True becuase True == True
+print(is_raining)                    # True
+print(is_raining == False)           # False
+
+print(has_red_shoes == True)         # False
+print(has_red_shoes == False)        # True they do not have red shoes
+
+print(sad)                           # False
+print(sad == True)                   # False
+print(sad == False)                  # True

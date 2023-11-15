638ad0d87ce19c819afc6393711236a533864903
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-11-15 14:28:45 -0500

    and not or with in and more substring

--- Unit Three - Conditional Statements in Python/Day 3 - Compound Boolean Expressions/and_or_not_examples.py
+++ Unit Three - Conditional Statements in Python/Day 3 - Compound Boolean Expressions/and_or_not_examples.py
@@ -5,6 +5,14 @@ y = 10
 if x > 0 and y > 0:
     print("Both x and y are greater than 0")
 
+
+
+if x > 0:
+    if y > 0:
+        print("Both x and y are greater than 0")
+
+
+        
 # Using 'or' to check multiple conditions
 if x > 0 or y > 0:
     print("At least one of x or y is greater than 0")

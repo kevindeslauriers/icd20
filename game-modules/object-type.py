6ef64abccdc34d6191698cfb9a10e5951ff4c8ba
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2024-03-01 14:52:42 -0500

    changed ' to " in printf

--- game-modules/object-type.py
+++ game-modules/object-type.py
@@ -6,7 +6,7 @@ name = {'lastname':'Harvey', 'firstname':'Steve'}
 address = {'street': '123 Sesame St','city':'Toronto', 'Province':'ON', 'Postal':'M3B3H7'}
 # print(address['street'])
 # print(address['city'])
-# print(f'{address['Province']}, {address['Postal']}')
+# print(f"{address['Province']}, {address['Postal']}")
 
 student = {'name':name, 'phone': '416-555-1212', 'student_no':'1126804', 'address':address}
 

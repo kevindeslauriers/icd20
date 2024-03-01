d9c60c0f083dae23ebc83cb32b4b2534b8024c65
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2024-03-01 14:46:39 -0500

    added address to student

--- game-modules/object-type.py
+++ game-modules/object-type.py
@@ -1,16 +1,14 @@
 name = {'lastname':'Harvey', 'firstname':'Steve'}
 
-print(name['firstname'])
-print(name['lastname'])
+# print(name['firstname'])
+# print(name['lastname'])
 
 address = {'street': '123 Sesame St','city':'Toronto', 'Province':'ON', 'Postal':'M3B3H7'}
-print(address['street'])
-print(address['city'])
-print(f'{address['Province']}, {address['Postal']}')
+# print(address['street'])
+# print(address['city'])
+# print(f'{address['Province']}, {address['Postal']}')
 
+student = {'name':name, 'phone': '416-555-1212', 'student_no':'1126804', 'address':address}
 
-
-# student = {'name':name, 'phone': '416-555-1212', 'student_no':'1126804'}
-
-# print(student['name']['lastname'])
+print(student['name']['lastname'])
 
